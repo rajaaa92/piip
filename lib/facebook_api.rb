193749a3e816de39f89496a767cd198b2d49da46
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-04-01 18:48:04 +0100

    fix reading

--- lib/facebook_api.rb
+++ lib/facebook_api.rb
@@ -1,20 +1,27 @@
 class FacebookApi
 
   def initialize(token, log = true)
-    put_log("Loggin in started")
-    @graph = Koala::Facebook::API.new(token)
-    put_log("Loggin in finished")
+    put_log("Logging in started")
+    @api = Koala::Facebook::API.new(token)
+    put_log("Logging in finished")
   end
 
   def post(text, log = true)
     put_log("Posting started") if log
-    @graph.put_connections("me", "feed", :message => text)
-    put_log("Posting finished") if log
+    resp = @api.put_wall_post(text)
+    put_log("Posting finished", "Post id: #{resp["id"]}") if log
   end
 
-  def put_log(label)
+  def read(posts_number, log = true)
+    put_log("Reading started") if log
+    statuses = @api.get_object("/me/statuses", "fields"=>"message", "limit" => posts_number)
+    put_log("Reading finished", "Read statuses: #{statuses.count}") if log
+  end
+
+  def put_log(label, body = nil)
     time = Time.now
-    puts "#{label}: #{time.strftime('%H:%M:%S.%3N')}"
+    body = "<#{body}>" if body
+    puts "#{label}: #{time.strftime('%H:%M:%S.%3N')} #{body}"
   end
 
 end
--- lib/piip-project.rb
+++ lib/piip-project.rb
@@ -4,15 +4,18 @@ Bundler.require
 
 require_relative "facebook_api"
 
-puts "Started."
+puts "Program started."
 
+# including config
 pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../../config.yml', __FILE__))).result) rescue {}) || {}
 AppConfig = Konf.new(pub_config)
 
-@api = FacebookApi.new(AppConfig.oauth_access_token)
 
-@api.post("I am writing on my wall again again wow serio niee!")
+@api = FacebookApi.new( AppConfig.oauth_access_token )
+@api.read( AppConfig.statuses_to_read_numer ) if AppConfig.reading_enabled
+@api.post( AppConfig.text_to_write + " #{Time.now}" ) if AppConfig.writing_enabled
+
 
 remove_instance_variable(:@api)
 
-puts "Finished."
+puts "Program finished."
