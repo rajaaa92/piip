0b81067115846e90bd8bab1c62c14a882bb01b35
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-05-15 16:29:13 +0100

    add iterations and time conditions to finish loop, clean up

--- lib/piip-project.rb
+++ lib/piip-project.rb
@@ -5,16 +5,17 @@ Bundler.require
 require_relative "facebook_api"
 require_relative "utilities"
 
-Utilities.start_program
+utilities = Utilities.new
 
 @api = FacebookApi.new( AppConfig.oauth_access_token )
-while !Utilities.exit_requested? do
+
+while !utilities.exit_requested? && utilities.iterations_condition && utilities.time_condition do
   @api.read( AppConfig.statuses_to_read_numer ) if AppConfig.reading_enabled
   @api.post( AppConfig.text_to_write + " #{Time.now}" ) if AppConfig.writing_enabled
-  Utilities.sleep_one_sec
+  utilities.sleep_one_sec
+  utilities.iteration_done
 end
 
 remove_instance_variable(:@api)
 
-Utilities.finish_program
-
+utilities.finish_program
--- lib/utilities.rb
+++ lib/utilities.rb
@@ -1,29 +1,42 @@
 pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../../config.yml', __FILE__))).result) rescue {}) || {}
-AppConfig = Konf.new(pub_config)
+sec_config = (YAML.load(ERB.new(File.read(File.expand_path('../../sec_config.yml', __FILE__))).result) rescue {}) || {}
+AppConfig = Konf.new(pub_config.merge(sec_config))
 
 class Utilities
 
-  @@exit_requested = false
-
-  class << self
-
-    def start_program
-      Kernel.trap( "INT" ) { @@exit_requested = true }
-      puts "Program started - press CTRL+C to exit."
-    end
-
-    def finish_program
-      puts "Program finished."
-    end
-
-    def sleep_one_sec
-      AppConfig.sleep_time.times { sleep 1 if !@@exit_requested }
-    end
-
-    def exit_requested?
-      @@exit_requested
-    end
-
+  def initialize
+    @iterations_done = 0
+    @start_time = Time.now
+    @exit_requested = false
+    start_program
   end
 
+  def start_program
+    Kernel.trap( "INT" ) { @exit_requested = true }
+    puts "Program started - press CTRL+C to exit."
+  end
+
+  def finish_program
+    puts "Program finished."
+  end
+
+  def sleep_one_sec
+    AppConfig.sleep_time.times { sleep 1 if !@exit_requested }
+  end
+
+  def exit_requested?
+    @exit_requested
+  end
+
+  def iterations_condition
+    @iterations_done < AppConfig.max_iterations
+  end
+
+  def iteration_done
+    @iterations_done += 1
+  end
+
+  def time_condition
+    @start_time > Time.now - AppConfig.max_time
+  end
 end
